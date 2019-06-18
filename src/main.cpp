2e903974b21155c7179c7ddab520f463582c4e1a
Kurtis Seebaldt <kseebaldt@gmail.com>
2019-06-18 13:58:45 -0500

    add pin mappings

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+
+#define DATA_PIN 16
+#define CLOCK_PIN 18
+#define LATCH_PIN 17
+
+#define DISPLAY_MODE_PIN 19
+
+#define CL1_PIN 25
+#define CL2_PIN 26
+
+#define R_LED_PIN 25
+#define G_LED_PIN 26
+#define B_LED_PIN 27
+
+#define I2C_SCL_PIN 22
+#define I2C_SDA_PIN 21
+
+void setup() {
+
+}
+
+void loop() {
+
+}
